4b5eb5b7159bbd3af589eb80392cf880075b2505
BENNATRAJ <18j039.bennatraj@gmail.com>
2024-08-06 17:40:09 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,49 @@
+
+
+import express from 'express'
+import cors from 'cors'
+import mysql from 'mysql'
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+const PORT = process.env.PORT || 4000
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'application',
+})
+
+db.connect(function (err) {
+    if (err) (console.log('Connecting error.'))
+    else { console.log('Connected Successfully') }
+})
+
+app.get('/registerdata', (req, res) => {
+    const sql = 'SELECT * FROM register'
+    db.query(sql, (err, result) => {
+        if (err) return res.json({ Message: 'Error. cannot connected' })
+        return res.json(result);
+    })
+})
+
+app.post('/register', (req, res) => {
+    const { name,mail,mobile } = req.body;
+    const sql = 'INSERT INTO register (name,mail,mobile) VALUES (?, ?, ?)';
+    db.query(sql, [name,mail,mobile], (err, result) => {
+        if (err) {
+            res.status(500).send('Form cannot submitted.')
+            console.log(err)
+            throw err;
+        } else {
+            res.send('Claim Data Submmited Successfully.')
+        }
+    })
+})
+
+
+app.listen(PORT, () => {
+    console.log('Listening Successfully')
+})
